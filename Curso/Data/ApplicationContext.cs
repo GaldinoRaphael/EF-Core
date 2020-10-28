203e414f5db13be56c392aad6a1881dfb4abf74b
GaldinoInovamobil <raphael.galdino@inovamobil.com.br>
2020-10-28 11:23:55 -0300

    Propriedades(CRUD) e bonus
    Propriedades do db context e bonus sobre configuração mapeamento de modelos esquecidos, multiplas tentativas de conexão e alteração do nome da tabela de migrações

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,23 +1,52 @@
 using System;
+using System.Linq;
 using CursoEFCore.Data.Configurations;
 using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CursoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
-        private string connectionString = "Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEfCore;Integrated Security=true";
+        //Informa qual o log que o EF vai utilizar para escrever os logs
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder
+            .UseLoggerFactory(_logger)
+            .EnableSensitiveDataLogging()
+            .UseSqlServer
+                (@"Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEfCore;Integrated Security=true",
+                     //Faz tentativas de conexões em caso de erro
+                     p => p.EnableRetryOnFailure
+                        (maxRetryCount: 2, maxRetryDelay: TimeSpan.FromSeconds(5), errorNumbersToAdd: null)
+                        //Muda o nome da tabela onde fica armazenados as histórias de migração
+                        .MigrationsHistoryTable("curso_ef_core"));
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Aplica as configurações de modelo em todas as classes
             //que extendem de Ientity no assembly (programa) ApplicationContext
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoItemConfiguration());
+            modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+
+            MapearPropriedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string));
+            }
         }
     }
 }
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CursoEFCore
 {
@@ -6,7 +9,142 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //InserirDados();
+            // InserirDadosEmMassa();
+            // ConsultarDados();
+            AtualizarDados();
+        }
+
+        // private static void ConsultarPedidoCarregamentoAdiantado()
+        // {
+        //     using (var db = new Data.ApplicationContext())
+        //     {
+        //         var pedidos = db
+        //             .Pedidos
+        //             //Inclui no carregamento adiantado os itens em pedido
+        //             .Include(p => p.Itens)
+        //                 //Inclui os pedidos que estão dentro de item
+        //                 .thenInclude(p => p.produto)
+        //                 .ToList();
+
+        //         Console.WriteLine(pedidos);
+        //     }
+        // }
+        public static void RemoverRegistros()
+        {
+            using (var db = new Data.ApplicationContext())
+            {
+                //var cliente = db.Clientes.Find(2);
+                //db.Clientes.Remove(cliente);
+                //db.Remove(cliente);
+                var cliente = new Cliente { Id = 1 };
+                db.Entry(cliente).State = EntityState.Deleted;
+
+                db.SaveChanges();
+            }
+        }
+        public static void AtualizarDados()
+        {
+            using (var db = new Data.ApplicationContext())
+            {
+                var cliente = db.Clientes.Find(1);
+                cliente.Nome = "Cliente Teste";
+                //db.Clientes.Update(cliente);
+                db.SaveChanges();
+
+            }
+
+            using (var db = new Data.ApplicationContext())
+            {
+                var cliente = db.Clientes.Find(1);
+
+                var clienteDesconectado = new
+                {
+                    Nome = "Cliente desconectado",
+                    Telefone = "12345648"
+                };
+
+                //Copia as propiedades do objeto passado no setvalues e coloca no objeto rastreado pelo entry
+                db.Entry(cliente).CurrentValues.SetValues(clienteDesconectado);
+            }
+
+        }
+        // private static void ConsultarDados()
+        // {
+        //     using (var db = new Data.ApplicationContext())
+        //     {
+        //         //var consultaPorSintaxe = (from c in db.Clientes where c.Id > 0 select c).ToList();
+        //         //AsNoTracking não gera o rastrio e portando não gera os arquivos em memória
+        //         var consultaPorMetodo = db.Clientes.AsNoTracking.FirstOfDefault(p => p.Id > 0).ToList();
+        //         foreach (var cliente in consultaPorMetodo)
+        //         {
+        //             //Procura primeiro por clientes em memória
+        //             db.Clientes.Find(cliente.id);
+        //         }
+        //     }
+        // }
+        private static void InserirDadosEmMassa()
+        {
+            var produtos = new[]
+            {
+                new Produto
+                {
+                    CodigoBarras = "123456",
+                    Ativo = true,
+                    Descricao = "Produto Teste",
+                    TipoProduto = ValueObjects.TipoProduto.MercadoriaParaRevenda,
+                    valor = 10m,
+                },
+                new Produto
+                {
+                    CodigoBarras = "123456",
+                    Ativo = true,
+                    Descricao = "Produto Teste",
+                    TipoProduto = ValueObjects.TipoProduto.MercadoriaParaRevenda,
+                    valor = 10m,
+                },
+                new Produto
+                {
+                    CodigoBarras = "123456",
+                    Ativo = true,
+                    Descricao = "Produto Teste",
+                    TipoProduto = ValueObjects.TipoProduto.MercadoriaParaRevenda,
+                    valor = 10m,
+                }
+            };
+
+            // var cliente = new Cliente
+            // {
+            //     Cep = "1235678",
+            //     Cidade = "Divinópolis",
+            //     Estado = "mg",
+            //     Nome = "joao",
+            //     Telefone = "99000001111",
+            // };
+            using var db = new Data.ApplicationContext();
+            // db.AddRange(produto, cliente);
+            db.AddRange(produtos);
+            var registros = db.SaveChanges();
+
+            Console.WriteLine($"O total de registros foi: {registros}");
+        }
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                CodigoBarras = "123456",
+                Ativo = true,
+                Descricao = "Produto Teste",
+                TipoProduto = ValueObjects.TipoProduto.MercadoriaParaRevenda,
+                valor = 10m
+            };
+
+            using var db = new Data.ApplicationContext();
+            //EF Rastrea na memória os daods
+            db.Produtos.Add(produto);
+            //Persiste na base de dados
+            var registros = db.SaveChanges();
+            Console.WriteLine($"O total de registros foi: {registros}");
         }
     }
 }
